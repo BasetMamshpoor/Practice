489d26f7d5043d3290bfbc82e5a36de9a07fa803
Baset mamshpoor <baset.jaldian@gmail.com>
2023-11-02 22:03:09 +0330

    sum 3 number

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b, c, sum;
+  cout << "Type 3 number: ";
+  cin >> a >> b >> c;
+  sum = a + b + c;
+  cout << a << "+" << b << "+" << c << "=" << sum;
+  return 0;
+}
